83a9c3a05702c90b66a4ba9ad46573323c360cae
ChurroDevelop <jhonfredy2325@gmail.com>
2024-07-18 17:34:53 -0500

    Agregando mas validaciones

--- web/scripts/login.js
+++ web/scripts/login.js
@@ -6,25 +6,32 @@ const $labelCorreo = document.querySelector("#labelCorreo");
 const $labelClave = document.querySelector("#labelClave");
 const $btnForm = document.querySelector("#btnForm");
 
-let inputTex = (event) => {
-    if (!/[A-Za-zàáâãéêíóôõúüñÑ\s]/.test(event.key)) {
-        event.preventDefault();
-    }
-};
-
-let inputNumber = (event) => {
-    if (!/[0-9]/.test(event.key)) {
-        event.preventDefault();
-    }
-};
-
-
+// Expresiones regulares para el manejo del formulario del login
+const regexAprendiz = /^[\w\.-]+@soy\.sena\.edu\.co$/;
+const regexInstructor = /^[\w\.-]+@sena\.edu\.co$/;
 
 let validarForm = (event) => {
-    let estadoForm = false;
+    let estadoForm = true;
+    
+    if ($inputCorreo.value.trim() === '') {
+        console.log("El correo es obligatorio");
+        $labelCorreo.classList.remove("border-green-500", "border-2");
+        $labelCorreo.classList.add("border-red-500", "border-2");
+        estadoForm = false;
+    }
+    else {
+//        if (!regexAprendiz.test($inputCorreo.value) || !regexInstructor.test($inputCorreo.value)) {
+//            console.log("Debe que ser un correo institucional");
+//            estadoForm = false;
+//        }
+    }
     
     if (!estadoForm) {
         event.preventDefault();
     }
+    else {
+        console.log("No se ha enviado el formulario");
+    }
 };
 
+$formulario.addEventListener("submit", validarForm);
