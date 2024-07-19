a891be19b283fd8dc257eaa0f31cf4bf53d7d4f1
ChurroDevelop <jhonfredy2325@gmail.com>
2024-07-19 09:15:51 -0500

    No tengo idea

--- web/scripts/login.js
+++ web/scripts/login.js
@@ -20,10 +20,23 @@ let validarForm = (event) => {
         estadoForm = false;
     }
     else {
-//        if (!regexAprendiz.test($inputCorreo.value) || !regexInstructor.test($inputCorreo.value)) {
-//            console.log("Debe que ser un correo institucional");
-//            estadoForm = false;
-//        }
+        if (!regexAprendiz.test($inputCorreo.value) || !regexInstructor.test($inputCorreo.value)) {
+            console.log("Debe que ser un correo institucional");
+            $labelCorreo.classList.remove("border-green-500", "border-2");
+            $labelCorreo.classList.add("border-red-500", "border-2");
+            estadoForm = false;
+        }
+        else {
+            $labelCorreo.classList.remove("border-red-500", "border-2");
+            $labelCorreo.classList.add("border-green-500", "border-2");
+        }
+    }
+    
+    if ($inputClave.value.trim() === '') {
+        console.log("La contraseña es obligatoria");
+        $labelClave.classList.remove("border-green-500", "border-2");
+        $labelClave.classList.add("border-red-500", "border-2");
+        estadoForm = false;
     }
     
     if (!estadoForm) {
